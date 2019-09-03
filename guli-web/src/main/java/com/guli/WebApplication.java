d1e71c98cf664a693df67d79f107d0f4ac303900
YTFar <2233618082@qq.com>
2019-09-03 10:53:08 +0800

    第二次提交(项目搭建2)

--- guli-web/src/main/java/com/guli/WebApplication.java
+++ guli-web/src/main/java/com/guli/WebApplication.java
@@ -2,10 +2,16 @@ package com.guli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication
 @EnableEurekaClient
+//开启Feign支持
+@EnableFeignClients
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
